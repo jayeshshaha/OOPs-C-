711aee6e6a0beb142f3bea4497b9793477a67b71
Jayesh Shaha <106771749+jayeshshaha@users.noreply.github.com>
2024-10-02 22:24:32 +0530

    left over

--- obj/Debug/net8.0/console-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/console-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("console-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e9b1e1290bf108cda1a42eb420233701b4f87c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77329078c5d7cc366b67ab014c988fb53c407943")]
 [assembly: System.Reflection.AssemblyProductAttribute("console-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("console-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
